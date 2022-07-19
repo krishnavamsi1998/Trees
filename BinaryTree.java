5965868eb72dc3216766057bbfa6df21dccee055
krishnavamsi1998 <106548869+krishnavamsi1998@users.noreply.github.com>
2022-07-19 13:21:49 +0530

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,5 +1,10 @@
 package trees;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 class Node
 {
 	int data;
@@ -21,40 +26,76 @@ public class BinaryTree {
 	
 	//left-root-right
 	
+	//O(N),O(N)
 	void inorder(Node node)
 	{
 		
-		if(node!=null)
-		{
+		if(node==null)
+			return;
+		
+		
 			inorder(node.left);
 			System.out.print(node.data+" ");
 			inorder(node.right);
-		}
+		
 	}
 	
+	//O(N),O(N)
 	void preorder(Node node)
 	{
+		if(node==null)
+			return;
 		
-		if(node!=null)
-		{
+		
 			System.out.print(node.data+" ");
 			preorder(node.left);
 			preorder(node.right);
-			
-			
-		}
+		
 	}
 	
+	//O(N),O(N)
 	void postorder(Node node)
 	{
 		
-		if(node!=null)
-		{		
+		if(node==null)
+			return;	
 			
 			postorder(node.left);
 			postorder(node.right);
 			System.out.print(node.data+" ");
+		
+	}
+	
+	//O(N),O(N)
+	
+	List<List<Integer>> levelorder(Node node) {
+		// store level wise nodes
+
+		List<List<Integer>> res = new ArrayList();
+
+		if (node == null)
+			return res;
+
+		Queue<Node> q = new LinkedList<>();
+
+		q.add(node);
+
+		while (!q.isEmpty()) {
+			int levels = q.size();
+			List<Integer> list = new ArrayList();
+			for (int i = 0; i < levels; i++) {
+				if (q.peek().left != null)
+					q.add(q.peek().left);
+				if (q.peek().right != null)
+					q.add(q.peek().right);
+				list.add(q.poll().data);
+
+			}
+			res.add(list);
 		}
+
+		return res;
+
 	}
 	
 	
@@ -71,6 +112,9 @@ public class BinaryTree {
 		tree.preorder(tree.root);
 		System.out.println("\nPOST-ORDER :");
 		tree.postorder(tree.root);
+		System.out.println("\nLEVEL-ORDER :");
+		
+		System.out.println(tree.levelorder(tree.root));
 		
 		
 	}
