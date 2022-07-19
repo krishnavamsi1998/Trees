6ebd6a1e3969b7ab628afd9527a5268a0c6c020f
krishnavamsi1998 <106548869+krishnavamsi1998@users.noreply.github.com>
2022-07-19 22:40:30 +0530

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 class Node
 {
@@ -92,12 +93,113 @@ public class BinaryTree {
 
 			}
 			res.add(list);
+			
 		}
 
 		return res;
 
 	}
 	
+	//O(n),O(n)
+	
+	List<List<Integer>> reverseLevelOrder(Node node) {
+		// store level wise nodes
+
+		List<List<Integer>> res = new ArrayList();
+		Stack<List<Integer>> stack=new Stack();
+		
+
+		if (node == null)
+			return res;
+
+		Queue<Node> q = new LinkedList<>();
+
+		q.add(node);
+
+		while (!q.isEmpty()) {
+			int levels = q.size();
+			List<Integer> list = new ArrayList();
+			for (int i = 0; i < levels; i++) {
+				if (q.peek().left != null)
+					q.add(q.peek().left);
+				if (q.peek().right != null)
+					q.add(q.peek().right);
+				list.add(q.poll().data);
+
+			}
+			stack.add(list);
+			
+		}
+		
+		while(!stack.isEmpty())
+		{
+			res.add(stack.pop());
+		}
+		return res;
+
+	}
+	
+	//O(n),O(n)
+	
+	List<Integer> preorderIterative(Node node) {
+		List<Integer> res = new ArrayList();
+
+		Stack<Node> stack = new Stack<>();
+
+		// root null
+		if (node == null)
+			return res;
+
+		stack.push(node);
+
+		while (!stack.isEmpty()) {
+			Node curr = stack.pop();
+			res.add(curr.data);
+			// reverse : right , left bcoz its stack
+			if (curr.right != null)
+				stack.push(curr.right);
+			if (curr.left != null)
+				stack.push(curr.left);
+
+		}
+
+		return res;
+	}
+	
+	//O(n,n)
+	
+	List<Integer> inorderIterative(Node node){
+		List<Integer> res = new ArrayList();
+
+		Stack<Node> stack = new Stack<>();
+
+		// root null
+		if (node == null)
+			return res;
+		Node curr=node;
+		
+		while(true)
+		{
+			if(curr!=null)
+			{
+				stack.push(curr);
+				curr=curr.left;
+				
+			}
+			//if left completed
+			else {
+				if(stack.isEmpty())
+					break;
+				//pop the root and push to res
+				curr=stack.pop();
+				res.add(curr.data);
+				//goto right
+				curr=curr.right;
+			}
+			
+		}
+		return res;
+	}
 	
 	public static void main(String[] args) {
 	
@@ -115,6 +217,16 @@ public class BinaryTree {
 		System.out.println("\nLEVEL-ORDER :");
 		
 		System.out.println(tree.levelorder(tree.root));
+		
+		System.out.println("\nREVERSE LEVEL-ORDER :");
+		
+		System.out.println(tree.reverseLevelOrder(tree.root));
+		
+		System.out.println("\nPRE-ORDER ITERATIVE :");
+		System.out.println(tree.preorderIterative(tree.root));
+		
+		System.out.println("\nIN-ORDER ITERATIVE :");
+		System.out.println(tree.inorderIterative(tree.root));
 		
 		
 	}
