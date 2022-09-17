b3a344888899cf563f7a15c175aed23eae5b8c6f
krishnavamsi1998 <106548869+krishnavamsi1998@users.noreply.github.com>
2022-09-17 23:01:21 +0530

    Create BST.java

---
+++ BST.java
@@ -0,0 +1,65 @@
+package trees;
+
+public class BST {
+
+	class Node {
+		int data;
+		Node left, right;
+
+		public Node(int key) {
+			this.data = key;
+			left = right = null;
+		}
+	}
+
+	Node root;
+
+	BST() {
+		root = null;
+
+	}
+
+	void insert(int key) {
+		root = insertKey(root, key);
+
+	}
+
+	// O(logn),O(n)
+	Node insertKey(Node root, int key) {
+
+		if (root == null) {
+			// using Node class which is already defined in other class
+			root = new Node(key);
+			return root;
+		}
+		if (key < root.data)
+			root.left = insertKey(root.left, key);
+		else if (key > root.data)
+			root.right = insertKey(root.right, key);
+
+		return root;
+	}
+
+	void inOrder(Node root) {
+		if (root == null)
+			return;
+		inOrder(root.left);
+		System.out.print(root.data + " ");
+		inOrder(root.right);
+	}
+
+	public static void main(String[] args) {
+		BST tree = new BST();
+
+		tree.insert(8);
+		tree.insert(3);
+		tree.insert(1);
+		tree.insert(6);
+		tree.insert(7);
+		tree.insert(10);
+		tree.insert(14);
+		tree.insert(4);
+
+		tree.inOrder(tree.root);
+	}
+}
